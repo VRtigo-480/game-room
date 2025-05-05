642a4ff1f488f513e40761b7b05320a0821d71ab
Josh Derrow <derrowjb@dukes.jmu.edu>
2025-05-05 14:52:09 -0400

    Merge Floating Objects to Main

--- Assets/Scripts/FloatingObjects.cs
+++ Assets/Scripts/FloatingObjects.cs
@@ -1,53 +1,127 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
-public class FloatingObjects : MonoBehaviour
+public class FloatingObjects : XRGrabInteractable
 {
-    private bool isHover = false; // Flag to check if the object is selected
     public float speed = 10.0f; // Speed of the floating object
     public Transform teleportTarget;    // Drag a destination GameObject here
     public GameObject xrRigObject;      // Drag your XR Origin here (e.g. "XR Origin (XR)")
+    private Vector3 originalScale;
+    public float hoverScaleMultiplier = 1.1f; // How much bigger when hovered
+    private Renderer objectRenderer;
+    public Color hoverColor = new Color(0f, 1f, 1f, 0.25f); // Semi-transparent cyan
+    private Material[] objectMaterials;
+    private Color[] originalColors;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    protected override void Awake()
+    {
+        base.Awake();
+        originalScale = transform.localScale;
+
+        objectRenderer = GetComponent<Renderer>();
+        if (objectRenderer != null)
+        {
+            objectMaterials = objectRenderer.materials;
+
+            // Save the original color for each material
+            originalColors = new Color[objectMaterials.Length];
+            for (int i = 0; i < objectMaterials.Length; i++)
+            {
+                if (objectMaterials[i].HasProperty("_Color"))
+                {
+                    originalColors[i] = objectMaterials[i].color;
+                }
+            }
+        }
+    }
+
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (!isHover) // If the object is not selected, make it rotate
-        {
-            transform.Rotate(0, speed * Time.deltaTime, 0); // Rotate the object around the Y-axis
-        }
-        else // If the object is selected, stop the rotation
-        {
-            transform.Rotate(0, 90, 0);
-        }
-    }
-
-    public void OnHover() // Method to be called when the object is selected
-    {
-        isHover = true; // Set the flag to true to stop rotation
-    }
-
-    public void OffHover() // Method to be called when the object is deselected
-    {
-        isHover = false; // Set the flag to false to resume rotation
+        transform.Rotate(0, speed * Time.deltaTime, 0); // Rotate the object around the Y-axis
     }
 
     public void Teleport()
     {
         Debug.Log("Teleport method called!");
 
+        // Force the interactor (controller) to drop this object BEFORE teleporting
+        if (isSelected)
+        {
+            interactionManager.SelectExit(firstInteractorSelecting, this);
+        }
+
         if (xrRigObject != null && teleportTarget != null)
         {
             xrRigObject.transform.position = teleportTarget.position;
+            xrRigObject.transform.rotation = teleportTarget.rotation;
             Debug.Log("Teleported to: " + teleportTarget.position);
         }
         else
         {
             Debug.LogWarning("Teleport target or XR Rig not assigned.");
+        }
+    }
+
+    // ====== XR Grab Override to Lock Position ======
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        // Lock attach transform so it doesn’t move
+        if (attachTransform != null)
+        {
+            attachTransform.position = transform.position;
+            attachTransform.rotation = transform.rotation;
+        }
+
+        // Call teleport when grabbed!
+        Teleport();
+
+        base.OnSelectEntered(args);
+    }
+
+    // ====== Pulse / Highlight on Hover ======
+    protected override void OnHoverEntered(HoverEnterEventArgs args)
+    {
+        base.OnHoverEntered(args);
+
+        // Scale up
+        transform.localScale = originalScale * hoverScaleMultiplier;
+
+        // Change all materials to hover color
+        if (objectMaterials != null)
+        {
+            foreach (var mat in objectMaterials)
+            {
+                if (mat.HasProperty("_Color"))
+                {
+                    mat.color = hoverColor;
+                }
+            }
+        }
+    }
+
+    protected override void OnHoverExited(HoverExitEventArgs args)
+    {
+        base.OnHoverExited(args);
+
+        // Reset scale
+        transform.localScale = originalScale;
+
+        // Reset each material to its saved original color
+        if (objectMaterials != null && originalColors != null)
+        {
+            for (int i = 0; i < objectMaterials.Length; i++)
+            {
+                if (objectMaterials[i].HasProperty("_Color"))
+                {
+                    objectMaterials[i].color = originalColors[i];
+                }
+            }
         }
     }
 }
